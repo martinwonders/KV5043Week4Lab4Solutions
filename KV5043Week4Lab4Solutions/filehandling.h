60c8104aea57eba3bd4445d0d13f4419bc0daf76
Endeavour <martin.wonders@unn.ac.uk>
2024-10-25 10:27:35 +0100

    add header guards and commented out pragma once

--- KV5043Week4Lab4Solutions/filehandling.h
+++ KV5043Week4Lab4Solutions/filehandling.h
@@ -1,4 +1,8 @@
-#pragma once
+//#pragma once
+#ifndef FILEHANDLING_H
+#define FILEHANDLING_H
 #include <string>
 
 void readFromFile(const std::string& fileName, std::string& fileContents);
+
+#endif //FILEHANDLING_H
